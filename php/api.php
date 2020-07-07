0d48554838326145e8d6ec3455d60ddb08b80aed
ActiveZ <activez.git@gmail.com>
2020-07-07 02:45:04 +0200

    supp header api

--- php/api.php
+++ php/api.php
@@ -1,8 +1,8 @@
 <?php
 //fichier sur le serveur
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTION');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, OPTION');
+// header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 //header('Access-Control-Allow-Headers: application/json');
 
 require_once 'bdd.php';
